4c795f9aee6718fdc2b7e3ffb786fc828fd36507
tanasi111 <ivantanasi111@gmail.com>
2019-08-28 20:34:39 +0200

    dev

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Post from '../../components/Post/Post';
 
 class Blog extends Component {
 
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+        const posts = axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                this.setState(
+                    {
+                        posts: response.data
+                    });
+                // console.log(response);
+            });
+    }
+
     render() {
+
+        const posts = this.state.posts.map(post => {
+            return <Post />
+        })
+
         return (
-            <div>Blog</div>
+            <div>{posts}</div>
         );
     }
 

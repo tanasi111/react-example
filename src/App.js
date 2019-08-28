d0149d4489c13a71f9174111c96d3c8e9304c7ac
tanasi111 <ivantanasi111@gmail.com>
2019-08-28 19:40:52 +0200

    dev

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 function App() {
   return (
     <div className="App">
       <h1>react app</h1>
+      <Person />
     </div>
   );
 }

da09a1d892f5aa165a97f8dafc4713e241981356
tanasi111 <ivantanasi111@gmail.com>
2019-08-28 20:06:11 +0200

    dev

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import Person from './components/Person/Person'
+import Person from './components/Person/Person';
+import Blog from './containers/Blog/Blog';
 
 function App() {
   return (
     <div className="App">
       <h1>react app</h1>
       <Person name="Ivan" />
+      <Blog />
     </div>
   );
 }
---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Blog extends Component {
+
+    render() {
+        return (
+            <div>Blog</div>
+        );
+    }
+
+}
+
+export default Blog;

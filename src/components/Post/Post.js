972e3efa589c7f359cc2c056b4770fe977e4961c
tanasi111 <ivantanasi111@gmail.com>
2019-08-29 11:46:50 +0200

    dev

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -2,13 +2,10 @@ import React from 'react';
 
 const post = (props) => (
     <article className="card">
-        {/* <p>{props.title}</p> */}
-
         <div className="card-body">
             <h5 className="card-title">{props.title}</h5>
             <p className="card-text">{props.body}</p>
         </div>
-
     </article>
 );
 

f4583ce3b1a4a0d61c0e4a2df65e13abaca941d4
tanasi111 <ivantanasi111@gmail.com>
2019-08-29 11:15:16 +0200

    navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 import Person from './components/Person/Person';
 import Blog from './containers/Blog/Blog';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>react app</h1>
       <Person name="Ivan" />
       <Blog />

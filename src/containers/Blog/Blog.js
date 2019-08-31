10f4ecd633d9085d80717c93d22ce89c5cf6afbe
tanasi111 <ivantanasi111@gmail.com>
2019-08-31 11:09:10 +0200

    dev

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './Blog.css';
 import Post from '../../components/Post/Post';
+import NewPost from '../../components/NewPost/NewPost'
 
 class Blog extends Component {
 
@@ -10,25 +11,45 @@ class Blog extends Component {
     }
 
     componentDidMount() {
-        const posts = axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                this.setState(
-                    {
-                        posts: response.data
-                    });
-                console.log(response);
+                const posts = response.data.slice(0, 4);
+                const updatePosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Ivan'
+                    }
+                });
+                this.setState({ posts: updatePosts });
+                console.log(updatePosts);
             });
     }
 
     render() {
 
-        const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} body={post.body} />
-        })
+        // const posts = this.state.posts.map(post => {
+        //     return <Post key={post.id} title={post.title} body={post.body} />
+        // })
+
+        let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>;
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)} />;
+            });
+        }
 
         return (
             <div>
-                <div className="posts">{posts}</div>
+                <section>
+                    <NewPost />
+                </section>
+                <section className="posts">
+                    {posts}
+                </section>
             </div>
         );
     }
